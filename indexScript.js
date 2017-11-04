ee769f6b022097a910f880ff7a516669c5fb4f88
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2017-11-04 11:41:26 +0800

    first commit

---
+++ indexScript.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){ {
+  //initial check to make sure jquery loaded
+  alert('jquery loaded');
+});
+
+  //initial check to make sure jquery loaded
+  //alert('jquery loaded');
