7d52212256b31197967c2474f98338897e678ce9
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2017-11-05 00:02:43 +0800

    added navbar

--- indexScript.js
+++ indexScript.js
@@ -1,8 +1,15 @@
 
-$(document).ready(function(){ {
-  //initial check to make sure jquery loaded
-  alert('jquery loaded');
-});
+$(document).ready(function(){
 
   //initial check to make sure jquery loaded
   //alert('jquery loaded');
+  $("span:parent").css({'color': '#fff', 'font-weight': 'bold', 'font-style': 'italic'});
+  $(".fa-home").css('color', '#fff');
+
+  $(".fa-home").hover(function(){
+    $(this).css("color", "green");
+    }, function(){
+    $(this).css("color", "#fff");
+});
+
+});
