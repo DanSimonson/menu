fb0a735c7785a3604a9695c25dfa8e208157e04f
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2017-11-05 09:55:34 +0800

    added slider images and navigation

--- indexScript.js
+++ indexScript.js
@@ -3,9 +3,12 @@ $(document).ready(function(){
 
   //initial check to make sure jquery loaded
   //alert('jquery loaded');
-  $("span:parent").css({'color': '#fff', 'font-weight': 'bold', 'font-style': 'italic'});
-  $(".fa-home").css('color', '#fff');
 
+  //set style for the navigation brand and home navigation icon
+  $("a:parent").css({'color': '#fff', 'font-weight': 'bold', 'font-style': 'italic'});
+  $(".fa-home").css({'color': '#fff','font-weight': 'bold', 'font-style': 'normal'});
+
+  //change fontawesome icon color on hover
   $(".fa-home").hover(function(){
     $(this).css("color", "green");
     }, function(){
